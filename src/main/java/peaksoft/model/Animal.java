b2d1bba0f55fd774f5b6fee0be97979c457457b4
sezim <sezimmarlisova@gmail.com>
2021-10-15 16:43:14 +0600

    practice1

--- src/main/java/peaksoft/model/Animal.java
+++ src/main/java/peaksoft/model/Animal.java
@@ -1,4 +1,30 @@
 package peaksoft.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Animal {
+    @Autowired
+    private AnimalsCage animalsCage;
+    @Autowired
+    private Dog dog1;
+
+    public Animal() {
+    }
+
+    public AnimalsCage getAnimalsCage() {
+        return animalsCage;
+    }
+
+    public void setAnimalsCage(AnimalsCage animalsCage) {
+        this.animalsCage = animalsCage;
+    }
+
+    public Dog getDog1() {
+        return dog1;
+    }
+
+    public void setDog1(Dog dog1) {
+        this.dog1 = dog1;
+    }
 }
+
--- src/main/java/peaksoft/model/Timer.java
+++ src/main/java/peaksoft/model/Timer.java
@@ -1,5 +1,9 @@
 package peaksoft.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
